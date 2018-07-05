4600f47bae35c13fc2baf9e2805586823050bde4
Joffrey Janiec <jjaniec@student.42.fr>
2018-07-05 18:41:58 +0200

    compile 21sh in travis before tests - add handle_redir_fd()

--- srcs/exec/handle_redirs.c
+++ srcs/exec/handle_redirs.c
@@ -6,21 +6,25 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 18:30:50 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/07/05 17:39:34 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/07/05 18:41:08 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <twenty_one_sh.h>
 
 /*
-static int		handle_redir_fd(int input_fd, int target_fd)
-{
-
-}
+** Handle redirections to fds ex: 2>&1
 */
 
+static void		handle_redir_fd(int input_fd, int target_fd)
+{
+	close(input_fd);
+	dup(target_fd);
+}
+
 /*
-** Handle redirections by replacing $input_fd target to $target_file
+** Handle redirections to files like '2>a.txt'
+** by replacing $input_fd target to $target_file
 ** by opening file and closing $input_fd with dup2
 */
 
@@ -37,19 +41,18 @@ static void		handle_redir_file(int input_fd, char *target_file)
 void			handle_redirs(t_ast *redir_ast_node)
 {
 	t_ast	*node;
+	int		input_fd;
 
 	node = redir_ast_node->parent;
 	log_trace("Handle redirs of %s(t %zu td %zu)", redir_ast_node->data[0], redir_ast_node->type, redir_ast_node->type_details);
 	log_trace("  Parent: t %zu td %zu", node->type, node->type_details);
+	input_fd = ft_atoi(node->data[0]);
 	while (node->type == T_REDIR_OPT)
 	{
-		/*
 		if (redir_ast_node->type_details == TK_LESSAND || redir_ast_node->type_details == TK_GREATAND)
-			handle_redir_fd();
-		else */
-			handle_redir_file(\
-				ft_atoi(node->data[0]), \
-				node->right->data[0]);
+			handle_redir_fd(input_fd, ft_atoi(node->right->data[0]));
+		else
+			handle_redir_file(input_fd, node->right->data[0]);
 		node = node->parent;
 	}
 }
