3c7e899d06ab4545163b0280b122bf16ad050a7a
Gamouche <cyril.f@hotmail.fr>
2018-07-14 16:17:36 +0200

    add new files for preparing future work

---
+++ srcs/line_edition/actionk_move_cursor_by_word_right.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actionk_move_cursor_by_word_right.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfermier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/14 16:00:54 by cfermier          #+#    #+#             */
+/*   Updated: 2018/07/14 16:00:54 by cfermier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/line_edition.h"
+
+void    actionk_move_cursor_by_word_right(struct s_line *le)
+{
+    (void) le;
+}
