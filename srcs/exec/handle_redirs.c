52f0b029416ea8fc7d6e70b5f59c57b08d60e40c
Joffrey Janiec <jjaniec@student.42.fr>
2018-07-14 17:30:12 +0200

    working beginning of redirections

--- srcs/exec/handle_redirs.c
+++ srcs/exec/handle_redirs.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 18:30:50 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/07/05 18:41:08 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/07/14 17:29:18 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 static void		handle_redir_fd(int input_fd, int target_fd)
 {
+	log_trace("  Redir fd %d -> %d(fd)", input_fd, target_fd);
 	close(input_fd);
 	dup(target_fd);
 }
@@ -32,7 +33,7 @@ static void		handle_redir_file(int input_fd, char *target_file)
 {
 	int fd;
 
-	log_trace("Redir fd %d -> %s", input_fd, target_file);
+	log_trace("  Redir fd %d -> %s(file)", input_fd, target_file);
 	fd = open(target_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	dup2(fd, input_fd);
 	close(fd);
@@ -45,14 +46,16 @@ void			handle_redirs(t_ast *redir_ast_node)
 
 	node = redir_ast_node->parent;
 	log_trace("Handle redirs of %s(t %zu td %zu)", redir_ast_node->data[0], redir_ast_node->type, redir_ast_node->type_details);
-	log_trace("  Parent: t %zu td %zu", node->type, node->type_details);
-	input_fd = ft_atoi(node->data[0]);
+	while (node && node->parent && node->parent->type == T_REDIR_OPT)
+		node = node->parent;
 	while (node->type == T_REDIR_OPT)
 	{
-		if (redir_ast_node->type_details == TK_LESSAND || redir_ast_node->type_details == TK_GREATAND)
+		log_trace("in while node: t %zu td %zu d %s d left %s d right %s", node->type, node->type_details, (node->data)[0], (node->left->data)[0], (node->right->data)[0]);
+		input_fd = ft_atoi(node->data[0]);
+		if (node->type_details == TK_LESSAND || node->type_details == TK_GREATAND)
 			handle_redir_fd(input_fd, ft_atoi(node->right->data[0]));
 		else
 			handle_redir_file(input_fd, node->right->data[0]);
-		node = node->parent;
+		node = node->left;
 	}
 }
