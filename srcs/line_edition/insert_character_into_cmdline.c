cbd3c0d369aaacaadb50838ecd6109e593dacd36
Gamouche <cyril.f@hotmail.fr>
2018-07-19 00:48:16 +0200

    rework insert_character_into_cmdline

--- srcs/line_edition/insert_character_into_cmdline.c
+++ srcs/line_edition/insert_character_into_cmdline.c
@@ -16,106 +16,92 @@ static void	insert_char_into_array(char *line, t_kno key, unsigned int pos)
 {
 	// check depassement tableau
 
-	fprintf(tty_debug, "BEFORE LINE = |%s|\n", line);
+	//fprintf(tty_debug, "BEFORE LINE = |%s|\n", line);
 
-	fprintf(tty_debug, "je bouge |%s|\n",   line + pos );
+	//fprintf(tty_debug, "je bouge |%s|\n",   line + pos );
 
 	ft_memmove(line + pos + 1, line + pos, ft_strlen(line + pos) );
 
 	line[pos] = key;
 
-	fprintf(tty_debug, "AFTER LINE  = |%s|\n", line);
+	//fprintf(tty_debug, "AFTER LINE  = |%s|\n", line);
+}
+
+static void	init_and_update_values(struct s_line *le, unsigned int *keep_pos,
+						unsigned int *keep_line,
+						unsigned int *keep_cursor_index_for_line)
+{
+	*keep_pos = le->current_cursor_pos;
+	*keep_line = le->current_cursor_line;
+	*keep_cursor_index_for_line = le->cursor_index_for_line;
+
+	++(le->line_index);
+	le->nb_li_currently_writing = le->current_cursor_line + 1;
+	le->line_index = le->cursor_index_for_line;
+	le->nb_car_written_on_last_current_line = (le->current_cursor_line == 0) \
+	? (le->cursor_index_for_line) : (le->current_cursor_pos);
+}
+
+static void	shift_printed_line(struct s_line *le)
+{
+	unsigned int	i_tmp;
+
+	i_tmp = le->cursor_index_for_line;
+	while (le->line[i_tmp] != '\0')
+	{
+		print_key_at_end(le, le->line[i_tmp]);
+		++i_tmp;
+	}
+}
+
+static void	move_cursor_back_to_right_place(struct s_line *le, unsigned int keep_pos, bool foo)
+{
+	bool	foo2;
+
+	foo2 = false;
+	while (le->current_cursor_pos - ((le->current_cursor_pos != 0) ? (1) : (0))
+	> keep_pos)
+	{
+		actionk_cursor_move_left(le);
+		foo2 = true;
+	}
+
+	if (foo == true)
+		actionk_cursor_move_left(le);
+	
+	if (foo2 == false)
+		while (le->current_cursor_pos < (keep_pos + 1))
+			actionk_cursor_move_right(le);
 }
 
 void		insert_character_into_cmdline(struct s_line *le, t_kno key)
 {
-	//fprintf(tty_debug, "INSERTION CARACTERE A LA POS %u\n", le->cursor_index_for_line);
-
 	unsigned int	keep_pos;
 	unsigned int	keep_line;
 	unsigned int	keep_cursor_index_for_line;
-
-	keep_pos = le->current_cursor_pos;
-	keep_line = le->current_cursor_line;
-	keep_cursor_index_for_line = le->cursor_index_for_line;
+	bool			foo;
 	
-										
 	insert_char_into_array(le->line, key, le->cursor_index_for_line);
-	++(le->line_index);
-
-	le->nb_li_currently_writing = le->current_cursor_line + 1;
-	le->line_index = le->cursor_index_for_line;
-
-	if (le->current_cursor_line == 0)
-	{
-		le->nb_car_written_on_last_current_line = le->cursor_index_for_line;
-	}
-	else
-	{
-		le->nb_car_written_on_last_current_line = le->current_cursor_pos;
-	}
-
-	fprintf(tty_debug, "current cursor pos ----- %u\n", le->current_cursor_pos);
-	fprintf(tty_debug, "1 ----- %u %u\n", le->current_cursor_line, keep_line);
-
-unsigned int x = le->cursor_index_for_line;
-char * _line = le->line;
-	while ( _line[x] != '\0' )
-	{
-		fprintf(tty_debug, "PRINT %c\n", _line[x]);
-		print_key_at_end(le, _line[x]);
-		++x;
-	}
-
-	if (  keep_pos == le->li_max_size - 1   )
-		keep_line ++;
-
-	fprintf(tty_debug, "2 ----- %u %u\n", le->current_cursor_line, keep_line);
-	int foo = 0;
-	int foo2 = 0;
-	while (1&&    le->current_cursor_line > keep_line)
-	{	//foo = 1;
-
-		//if (  keep_pos == le->li_max_size - 1   )
-		//	{ fprintf(tty_debug, "BREAK BREAK\n"); break ; }
-
-		fprintf(tty_debug, "MONTE %u %u\n", keep_pos, le->li_max_size);
+	init_and_update_values(le, &keep_pos, &keep_line, &keep_cursor_index_for_line);
+	shift_printed_line(le);
+	if (keep_pos == le->li_max_size - 1)
+		keep_line += 1;
+	while (le->current_cursor_line > keep_line)
+	{	
 		tputs(le->tcaps->up, 1, &write_one_char);
 		--(le->current_cursor_line);
 		le->cursor_index_for_line -= le->li_max_size - 0;
 	}
-
-	
-	//sleep(5);
-
+	foo = false;
 	if (keep_pos == le->li_max_size - 1)
-		{ fprintf(tty_debug, "CE IF\n");   keep_pos = 0; foo = 1; }
-
-	fprintf(tty_debug, "while %u %u\n", le->current_cursor_pos, keep_pos );
-
-	while (
-	(le->current_cursor_pos - ( (le->current_cursor_pos != 0) ? 1 : 0   )) 
-	> keep_pos
-	)
-		{  /*fprintf(tty_debug, "left\n");*/ actionk_cursor_move_left(le); foo2 = 1; }
-
-
-	if (foo) actionk_cursor_move_left(le);
-
-	
-	while ( (foo2 == 0) &&  le->current_cursor_pos  < ( keep_pos + 1 )     )
-		{ /*fprintf(tty_debug, "right\n");*/ 
-			
-			//if (  foo && (le->current_cursor_pos == le->li_max_size - 1)   )
-			//	{ fprintf(tty_debug, "BREAK\n");  break ; }
-
-			actionk_cursor_move_right(le); 
-		
-		}
-
+	{
+		keep_pos = 0;
+		foo = true;
+	}
+	move_cursor_back_to_right_place(le, keep_pos, foo);
 }
 
-void		_insert_character_into_cmdline(struct s_line *le, t_kno key)
+void		old_version_insert_character_into_cmdline(struct s_line *le, t_kno key)
 {
 	//fprintf(tty_debug, "INSERTION CARACTERE A LA POS %u\n", le->cursor_index_for_line);
 
