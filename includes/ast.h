addce0dd73840862a9ba2330a47530d26ad37f36
sbrucker <sbrucker@student.42.fr>
2018-07-22 16:53:31 +0200

    Priority of pipes

--- includes/ast.h
+++ includes/ast.h
@@ -6,7 +6,7 @@
 /*   By: sbrucker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/21 15:06:00 by sbrucker          #+#    #+#             */
-/*   Updated: 2018/07/19 13:48:27 by sbrucker         ###   ########.fr       */
+/*   Updated: 2018/07/22 16:34:36 by sebastien        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,12 @@ void	ast_debug(t_ast *root);
 
 int		is_op0(t_lexeme *lex);
 int		is_op1(t_lexeme *lex);
+int		is_op1_5(t_lexeme *lex);
 int		is_op2(t_lexeme *lex);
 int		is_op3(t_lexeme *lex);
 int		is_nodeop0(t_ast *ast);
 int		is_nodeop1(t_ast *ast);
+int		is_nodeop1_5(t_ast *ast);
 int		is_nodeop2(t_ast *ast);
 int		is_nodeop3(t_ast *ast);
 
--- srcs/ast/is_op.c
+++ srcs/ast/is_op.c
@@ -6,7 +6,7 @@
 /*   By: sbrucker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/19 15:09:13 by sbrucker          #+#    #+#             */
-/*   Updated: 2018/07/19 15:09:21 by sbrucker         ###   ########.fr       */
+/*   Updated: 2018/07/22 16:34:00 by sebastien        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,15 @@ inline int	is_op1(t_lexeme *lex)
 	|| lex->type_details == TK_OR);
 }
 
+inline int	is_op1_5(t_lexeme *lex)
+{
+	return (lex->type_details == TK_PIPE);
+}
+
 inline int	is_op2(t_lexeme *lex)
 {
-	return (lex->type_details == TK_PIPE
-	|| (lex->type == T_REDIR_OPT
+	return (lex->type_details != TK_PIPE
+	&& (lex->type == T_REDIR_OPT
 		&& lex->type_details != TK_DLESS
 		&& lex->type_details != TK_DLESSDASH));
 }
