220e97e4b2ffa91503597399aeda624b07ea4f97
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-07-23 11:49:30 +0200

    Mise au propre du main

--- srcs/main.c
+++ srcs/main.c
@@ -6,40 +6,41 @@
 /*   By: cyfermie <cyfermie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 16:19:06 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/07/19 16:22:16 by cyfermie         ###   ########.fr       */
+/*   Updated: 2018/07/23 11:47:55 by sbrucker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <twenty_one_sh.h>
 
-static void lexer_loop(void)
+static void twentyonesh(char **envp)
 {
-  t_lexeme	*lex;
-	char	*input;
+	t_lexeme	*lex;
+	char		*input;
 
-tty_debug = fopen(TTY_DEBUG, "w");
+	(void)envp;
+	tty_debug = fopen(TTY_DEBUG, "w");
 	while (1)
 	{
 		ft_putstr("%> ");
 		input = line_edition();
 		lex = lexer(input);
-   		 ast(lex);
+		ast(lex);
+		free(input);
 	}
-	free(input);
 }
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **envp)
 {
-  t_lexeme	*lex;
-  
-  if (!DEBUG)
+	t_lexeme	*lex;
+
+	if (!DEBUG)
 		log_set_quiet(1);
 	if (ac > 1)
-  {
+	{
 		lex = lexer(ft_strdup(av[1]));
-	  ast(lex);
-  }
+		ast(lex);
+	}
 	else
-		lexer_loop();
+		twentyonesh(envp);
 	return (0);
 }
