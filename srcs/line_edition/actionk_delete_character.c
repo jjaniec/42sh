bb1dd4ac06111591c7490308f3679f09a04275de
Gamouche <cyril.f@hotmail.fr>
2018-08-20 21:29:04 +0200

    oublie un fichier

--- srcs/line_edition/actionk_delete_character.c
+++ srcs/line_edition/actionk_delete_character.c
@@ -6,7 +6,7 @@
 /*   By: cyfermie <cyfermie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/19 14:53:51 by cyfermie          #+#    #+#             */
-/*   Updated: 2018/08/05 20:43:45 by cyfermie         ###   ########.fr       */
+/*   Updated: 2018/08/20 19:14:41 by cyfermie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	delete_last_char_while_moving_back_cursor(struct s_line *le)
 	keep_current_cursor_line = le->current_cursor_line;
 	actionk_cursor_move_left(le);
 	tputs(le->tcaps->dc, 1, &write_one_char);
-	if (le->current_cursor_pos == le->term_line_size - 1)
+	if (cursor_is_at_end_of_term_line(le->current_cursor_pos, le) == true)
 		weird_trick_to_erase_char(le);
 	--(le->line_index);
 	le->line[le->line_index] = '\0';
@@ -41,18 +41,18 @@ static void	delete_last_char_while_moving_back_cursor(struct s_line *le)
 static void	delete_last_char_without_moving_cursor(struct s_line *le)
 {
 	tputs(le->tcaps->dc, 1, &write_one_char);
-	if (le->current_cursor_pos == le->term_line_size - 1)
+	if (cursor_is_at_end_of_term_line(le->current_cursor_pos, le) == true)
 		weird_trick_to_erase_char(le);
 	--(le->line_index);
 	le->line[le->line_index] = '\0';
-	if (le->current_cursor_pos == (le->term_line_size - 1))
+	if (cursor_is_at_end_of_term_line(le->current_cursor_pos, le) == true)
 	{
 		--(le->nb_li_currently_writing);
 		le->nb_car_written_on_last_current_line = le->term_line_size - 1;
 		if (!(le->current_cursor_line) && le->nb_li_currently_writing == 1)
 			le->nb_car_written_on_last_current_line = le->term_line_size \
 			- le->start_pos - 1;
-		else if (le->current_cursor_pos != le->term_line_size - 1)
+		else if (cursor_is_at_end_of_term_line(le->current_cursor_pos, le) == false)
 		{
 			//fprintf(tty_debug, "CE ELSE - - - - - - - - - - - - \n");
 			--(le->nb_car_written_on_last_current_line);
@@ -72,16 +72,23 @@ void		actionk_delete_character(struct s_line *le)
 	{
 		if (le->current_cursor_line == 0
 		&& le->current_cursor_pos == le->start_pos)
+		{
+			fprintf(tty_debug, "NOP PAS DE BACKSPACE\n");
 			return ;
-		if (cursor_is_at_end(le) == true)
+		}
+		if (cursor_is_at_end_of_cmd(le) == true)
 			delete_last_char_while_moving_back_cursor(le);
 		else
 			delete_char_into_cmdline_while_moving_back_cursor(le);
 	}
 	else if (le->key_no == LE_DELETE)
 	{
+		//if (cursor_is_at_end_of_cmd(le))
 		if (le->cursor_index_for_line == le->line_index)
+		{
+			fprintf(tty_debug, "NOP PAS DE DELETE\n");
 			return ;
+		}
 		if (le->cursor_index_for_line == (le->line_index - 1))
 		{
 			delete_last_char_without_moving_cursor(le);
