e5f482fb81681c2b4884408fc274995dd449923b
Joffrey Janiec <jjaniec@student.42.fr>
2018-08-29 19:13:24 +0200

    Use a temporary file instead of /tmp in COL_PROG_ARG_FILE tests

--- tests/syntax_highlighting_tests.c
+++ tests/syntax_highlighting_tests.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 21:10:22 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/08/29 18:52:15 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/08/29 19:11:30 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,13 +66,15 @@ void			syntax_highlighting_tests(char **envp)
 	remove("/tmp/thisdoesnotexists");
 	test_syntax_hightlighting("Quoted args 1", "ls -\\\" \" arg \"   '  arg  ' \" arg\"'arg ' ||ls_", COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_PROG_OPT" -\\\""COL_DEFAULT COL_QUOTED_ARG" \" arg \""COL_DEFAULT\
 		COL_QUOTED_ARG"   '  arg  '"COL_DEFAULT COL_QUOTED_ARG" \" arg\"'arg '"COL_DEFAULT COL_OPERATORS" ||"COL_DEFAULT COL_PROG_NAME_NOT_FOUND"ls_"COL_DEFAULT"\n");
-	test_syntax_hightlighting("Long 1 - prog not found - whitespaces - multiple options - redirs", "ls___      -a -b -c   \t-d\t  \t-- /tmp ./srcs 2>/dev/null 2>&1", COL_PROG_NAME_NOT_FOUND"ls___"COL_DEFAULT\
-		COL_PROG_OPT"      -a"COL_DEFAULT COL_PROG_OPT" -b"COL_DEFAULT COL_PROG_OPT" -c"COL_DEFAULT COL_PROG_OPT"   \t-d"COL_DEFAULT COL_PROG_OPT"\t  \t--"COL_DEFAULT COL_PROG_ARG_FILE" /tmp"COL_DEFAULT\
+	close(open("./temp_file", O_WRONLY | O_CREAT | O_TRUNC, 0666));
+	test_syntax_hightlighting("Long 1 - prog not found - whitespaces - multiple options - redirs", "ls___      -a -b -c   \t-d\t  \t-- ./temp_file ./srcs 2>/dev/null 2>&1", COL_PROG_NAME_NOT_FOUND"ls___"COL_DEFAULT\
+		COL_PROG_OPT"      -a"COL_DEFAULT COL_PROG_OPT" -b"COL_DEFAULT COL_PROG_OPT" -c"COL_DEFAULT COL_PROG_OPT"   \t-d"COL_DEFAULT COL_PROG_OPT"\t  \t--"COL_DEFAULT COL_PROG_ARG_FILE" ./temp_file"COL_DEFAULT\
 		COL_PROG_ARG_DIR" ./srcs"COL_DEFAULT COL_REDIRS" 2>"COL_DEFAULT COL_PROG_ARG_FILE"/dev/null"COL_DEFAULT COL_REDIRS" 2>&"COL_DEFAULT COL_PROG_ARG_NOT_FOUND"1"COL_DEFAULT"\n");
-	test_syntax_hightlighting("Long 2 - w/ operators", "ls -la /tmp; ls; ls -la;ls ;   ls;", COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_PROG_OPT" -la"COL_DEFAULT COL_PROG_ARG_FILE" /tmp"COL_DEFAULT\
+	test_syntax_hightlighting("Long 2 - w/ operators", "ls -la ./temp_file; ls; ls -la;ls ;   ls;", COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_PROG_OPT" -la"COL_DEFAULT COL_PROG_ARG_FILE" ./temp_file"COL_DEFAULT\
 		COL_OPERATORS";"COL_DEFAULT COL_PROG_NAME_FOUND" ls"COL_DEFAULT COL_OPERATORS";"COL_DEFAULT COL_PROG_NAME_FOUND" ls"COL_DEFAULT COL_PROG_OPT" -la"COL_DEFAULT COL_OPERATORS";"COL_DEFAULT\
 		COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_OPERATORS" ;"COL_DEFAULT COL_PROG_NAME_FOUND"   ls"COL_DEFAULT COL_OPERATORS";"COL_DEFAULT"\n");
-	test_syntax_hightlighting("Long 3 - operators - env expansions", "ls -la $PWD /tmp $HOME ||ls|| ls&&ls_", COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_PROG_OPT" -la"COL_DEFAULT COL_ENV_VAR_EXPANSION" $PWD"COL_DEFAULT\
-		COL_PROG_ARG_FILE" /tmp"COL_DEFAULT COL_ENV_VAR_EXPANSION" $HOME"COL_DEFAULT COL_OPERATORS" ||"COL_DEFAULT COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_OPERATORS"||"COL_DEFAULT COL_PROG_NAME_FOUND" ls"COL_DEFAULT\
+	test_syntax_hightlighting("Long 3 - operators - env expansions", "ls -la $PWD ./temp_file $HOME ||ls|| ls&&ls_", COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_PROG_OPT" -la"COL_DEFAULT COL_ENV_VAR_EXPANSION" $PWD"COL_DEFAULT\
+		COL_PROG_ARG_FILE" ./temp_file"COL_DEFAULT COL_ENV_VAR_EXPANSION" $HOME"COL_DEFAULT COL_OPERATORS" ||"COL_DEFAULT COL_PROG_NAME_FOUND"ls"COL_DEFAULT COL_OPERATORS"||"COL_DEFAULT COL_PROG_NAME_FOUND" ls"COL_DEFAULT\
 		COL_OPERATORS"&&"COL_DEFAULT COL_PROG_NAME_NOT_FOUND"ls_"COL_DEFAULT"\n");
+	remove("./temp_file");
 }
