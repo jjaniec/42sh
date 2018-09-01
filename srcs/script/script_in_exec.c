03fa88d4a633526d0cfce01295bdf57c7ef81013
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-09-01 13:24:19 +0200

    Infinite while loop works with [ 0 ] or [ 1 ]

--- srcs/script/script_in_exec.c
+++ srcs/script/script_in_exec.c
@@ -6,7 +6,7 @@
 /*   By: sbrucker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 13:00:01 by sbrucker          #+#    #+#             */
-/*   Updated: 2018/09/01 13:16:19 by sbrucker         ###   ########.fr       */
+/*   Updated: 2018/09/01 13:22:14 by sbrucker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,27 @@
 
 void	script_in_exec(t_ast *node, t_exec *exe)
 {
+	t_ast	*script_node;
+
+	script_node = node->sub_ast->left;
 	//Go to the condition
-	node = node->sub_ast->left->left;
+	script_node = script_node->left;
 	//Downgrade of the condition
-	node->type = T_WORD;
-	node->type_details = TK_DEFAULT;
+	script_node->type = T_WORD;
+	script_node->type_details = TK_DEFAULT;
 	log_info("Downgrade of conditionnal node in AST into type T_WORD.");
-	in_exec(node, exe);
-	node = node->parent;
+	in_exec(script_node, exe);
+	script_node = script_node->parent;
 	//If the script is an IF
-	if (node->type_details == TK_SCRIPT_IF)
+	if (script_node->type_details == TK_SCRIPT_IF)
 	{
 		if (exe->ret == 0)
-			exe = ast_explore(node->right->sub_ast, exe);
+			exe = ast_explore(script_node->right->sub_ast, exe);
+	}
+	if (script_node->type_details == TK_SCRIPT_WHILE)
+	{
+		if (exe->ret == 0)
+			exe = ast_explore(script_node->right->sub_ast, exe);
+		script_in_exec(node, exe);
 	}
 }
