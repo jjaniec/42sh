f453f1245a8347ce0e8cb947d7e0cf532dd668b6
Cyril Fermier <cyril.f@hotmail.fr>
2018-09-15 16:04:44 +0200

    nothing, just changing branch for a test

--- includes/line_edition.h
+++ includes/line_edition.h
@@ -6,7 +6,7 @@
 /*   By: cyfermie <cyfermie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 15:45:45 by cyfermie          #+#    #+#             */
-/*   Updated: 2018/09/15 15:55:59 by cyfermie         ###   ########.fr       */
+/*   Updated: 2018/09/15 16:04:00 by cyfermie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -306,11 +306,8 @@ void    actionk_history_down(struct s_line *le);
 
 
 /*
-	 `Backslash a la fin de la
-	 ligne pour echapper le \n` - `Completer des quotes, simple-, double- and back- quotes`
 
-
-	AND et OR et HEREDOC
+	POUR TOUS LES PROMPTS (ctrl d)
 	{
 		si cmd vide = nothing happens
 		si cmd est pas vide, et curseur est au bout = nothing happens
