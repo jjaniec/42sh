d4495f2fd0ce00ac8ce4efc1790240d8f5b96739
Joffrey Janiec <jjaniec@student.42.fr>
2018-09-18 19:43:48 +0200

    Working version of handle_expansions w/o handling of quotes & escape chars

--- srcs/lexer/handle_expansions.c
+++ srcs/lexer/handle_expansions.c
@@ -6,30 +6,91 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 16:01:23 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/09/18 17:36:44 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/09/18 19:36:45 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <twenty_one_sh.h>
 
+static char		*get_expansion_end(char *str, char *lexeme_end_ptr)
+{
+	char		*r;
+
+	r = str + 1;
+	while (*r && *r != '$' && r != lexeme_end_ptr)
+		r++;
+	log_fatal("exp end *%s", r);
+	return (r);
+}
+
+static char		*concat_expansion(t_lexeme *lexeme, char *expansion_char_pos, \
+					char *expansion_data, char *env_val)
+{
+	//char	*expansion_end;
+	char	*expansion_prefix_data;
+	char	*expansion_suffix_data;
+	char	*ret;
+	char	*ptr;
+	int		i;
+(void)expansion_data;
+	expansion_prefix_data = lexeme->data;
+	log_debug("exp prefix data %s", expansion_prefix_data);
+	expansion_suffix_data = get_expansion_end(expansion_char_pos, lexeme->lexeme_end_ptr);
+	log_debug("exp suffix data %s", expansion_suffix_data);
+
+	ret = malloc(sizeof(char) * ft_strlen(lexeme->data) - ft_strlen(expansion_char_pos) + ft_strlen(env_val) + 1);
+	ptr = expansion_prefix_data;
+	i = 0;
+	log_info("exp char pos : %s", expansion_char_pos);
+	while (ptr && *ptr && *ptr != *expansion_char_pos)
+		ret[i++] = *(ptr++);
+	log_info("Stop 1st while %s", ret);
+	while (env_val && *env_val)
+		ret[i++] = *(env_val++);
+	ptr = expansion_suffix_data;
+	while (ptr && *ptr)
+		ret[i++] = *(ptr++);
+	ret[i] = '\0';
+	return (ret);
+}
+
 static void		handle_dollar_expansion(t_lexeme *lexeme, char *expansion_char_pos, char **env)
 {
 	char		*new_data;
-	char		lexeme_end_char;
+	char		exp_end_char;
 	char		*env_val;
+	char		*exp_end;
 
 	log_fatal("Expansion char pos : |%s| end |%s|", expansion_char_pos, lexeme->lexeme_end_ptr);
 
-	lexeme_end_char = *(lexeme->lexeme_end_ptr);
-	*(lexeme->lexeme_end_ptr) = '\0';
+	exp_end = get_expansion_end(expansion_char_pos, lexeme->lexeme_end_ptr); //*(lexeme->lexeme_end_ptr);
+	exp_end_char = *(exp_end);
+	*(exp_end) = '\0';
 	env_val = get_env(expansion_char_pos + 1, (const char **)env);
 	if (env_val)
+	{
 		new_data = ft_strdup(env_val);
+		if (lexeme->lexeme_begin_ptr != expansion_char_pos)
+			new_data = concat_expansion(lexeme, expansion_char_pos, new_data, env_val);
+	}
 	else
+	{/*
+		if (lexeme->next)
+		{
+			if (lexeme->prev)
+				lexeme->next->prev = lexeme->prev;
+			else
+				lexeme->next->prev = NULL;
+		}*/
+/*
+		if (lexeme->lexeme_begin_ptr != expansion_char_pos)
+			new_data = concat_expansion(lexeme, new_data);*/
 		new_data = ft_strdup("");
+	}
 	free(lexeme->data);
 	lexeme->data = new_data;
-	*(lexeme->lexeme_end_ptr) = lexeme_end_char;
+	*(exp_end) = exp_end_char;
+	log_info("lexeme data replaced w/ : %s", lexeme->data);
 }
 
 static int		is_expansion_char(char c, void **callback)
@@ -51,7 +112,6 @@ static void		handle_lexeme_expansions(t_lexeme *lexeme, char **env)
 
 	i = 0;
 	in_quote_type = NOT_IN_QUOTES;
-	log_fatal("handle exp of : |%s|, lexeme->begin[0] |%c|", lexeme->data, lexeme->lexeme_begin_ptr[i]);
 	while (lexeme->lexeme_begin_ptr[i] && \
 		(lexeme->lexeme_begin_ptr + i) != lexeme->lexeme_end_ptr)
 	{
@@ -63,6 +123,8 @@ static void		handle_lexeme_expansions(t_lexeme *lexeme, char **env)
 		}
 		else if (is_expansion_char(lexeme->lexeme_begin_ptr[i], &expansion_handler))
 		{
+			log_fatal("handle exp of : |%s|, lexeme->begin[0] |%c|", lexeme->lexeme_begin_ptr + i, lexeme->lexeme_begin_ptr[i]);
+
 			(*(void (*)(t_lexeme *, char *, char **))(expansion_handler))\
 				(lexeme, lexeme->lexeme_begin_ptr + i, env);
 		}
