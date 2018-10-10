0c12f9eb06da384922ca695bb31c236dba422d3e
Cyril Fermier <cyril.f@hotmail.fr>
2018-10-10 17:08:49 +0200

    now we can delete alias with option -d

--- srcs/builtin/builtin_alias.c
+++ srcs/builtin/builtin_alias.c
@@ -6,7 +6,7 @@
 /*   By: cyfermie <cyfermie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/07 15:06:12 by cyfermie          #+#    #+#             */
-/*   Updated: 2018/10/09 16:14:36 by cyfermie         ###   ########.fr       */
+/*   Updated: 2018/10/10 17:08:00 by cyfermie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,22 @@ static bool			save_aliases_in_file(struct s_alias *alias)
 
 	if (check_backup_file(aliases_file_path) == false)
 		return (false);
-	if ((fd = open(aliases_file_path, O_WRONLY | O_TRUNC)) == -1)
-	{
-		ft_putstr_fd("42sh: error with file .42sh_aliases\n", STDERR_FILENO);
+	if ((fd = open(aliases_file_path, O_WRONLY | O_TRUNC)) == -1
+	&& write(2, "42sh: error with file .42sh_aliases\n", 36))
 		return (false);
-	}
 	if (alias->key == NULL)
 		return (true);
 	while (alias != NULL)
 	{
-		if (write(fd, alias->key, ft_strlen(alias->key)) == (ssize_t)(-1)
-		|| write(fd, " ", sizeof(char)) == (ssize_t)(-1)
-		|| write(fd, alias->value, ft_strlen(alias->value)) == (ssize_t)(-1)
-		|| write(fd, "\n", sizeof(char)) == (ssize_t)(-1))
-		{
-			ft_putstr_fd(".42sh_aliases: error writing in file\n", 2);
-			return (close(fd) ? (false) : (false));
-		}
+		if (ft_strlen(alias->key) > 0U)
+			if (write(fd, alias->key, ft_strlen(alias->key)) == (ssize_t)(-1)
+			|| write(fd, " ", sizeof(char)) == (ssize_t)(-1)
+			|| write(fd, alias->value, ft_strlen(alias->value)) == (ssize_t)(-1)
+			|| write(fd, "\n", sizeof(char)) == (ssize_t)(-1))
+			{
+				ft_putstr_fd(".42sh_aliases: error writing in file\n", 2);
+				return (close(fd) ? (false) : (false));
+			}
 		alias = alias->next;
 	}
 	return (close(fd) ? (true) : (true));
@@ -103,7 +102,39 @@ static void			print_aliases(struct s_alias *alias)
 		return ;
 	while (alias != NULL)
 	{
-		ft_printf("'%s' = '%s'\n", alias->key, alias->value);
+		if (ft_strlen(alias->key) > 0U)
+			ft_printf("'%s' = '%s'\n", alias->key, alias->value);
+		alias = alias->next;
+	}
+}
+
+static bool			check_option(const char *arg)
+{
+	if (ft_strequ(arg, "-d"))
+		return (true);
+	if (arg[0] == '-')
+		return (false);
+	while (*arg != '\0' && is_separator(*arg))
+		++arg;
+	if (*arg == '-')
+		return (false);
+	return (true);
+}
+
+static void			delete_alias(const char *key, struct s_alias *alias)
+{
+	if (alias->key == NULL && "list is empty")
+		return ;
+	if (ft_strlen(key) == 0)
+		return ;
+	while (alias != NULL)
+	{
+		if (ft_strequ(key, alias->key))
+		{
+			alias->key[0] = '\0';
+			alias->value[0] = '\0';
+			return ;
+		}
 		alias = alias->next;
 	}
 }
@@ -116,10 +147,12 @@ void				builtin_alias(char **argv, char **envp, t_exec *exe)
 	exe->ret = ((envp) ? (0) : (0));
 	alias = access_alias_datas();
 	nb_args = count_elem_2d_array(argv + 1);
-	if (nb_args != 0 && nb_args != 1 && nb_args != 2)
+	if ((nb_args != 0 && nb_args != 1 && nb_args != 2) \
+	|| (nb_args == 2 && check_option(argv[1]) == false))
 	{
 		ft_putstr_fd(BUILTIN_ALIAS_USAGE, STDERR_FILENO);
 		exe->ret = 1;
+		return ;
 	}
 	if (nb_args == 0)
 		print_aliases(alias);
@@ -134,5 +167,10 @@ void				builtin_alias(char **argv, char **envp, t_exec *exe)
 			exe->ret = save_aliases_in_file(alias) == true ? (0) : (1);
 	}
 	else if (nb_args == 2)
-		exe->ret = add_tmp_alias(argv[1], argv[2], alias) == true ? (0) : (1);
+	{
+		if (ft_strequ(argv[1], "-d"))
+			delete_alias(argv[2], alias);
+		else
+			exe->ret = add_tmp_alias(argv[1], argv[2], alias) == true ? 0 : 1;
+	}
 }
