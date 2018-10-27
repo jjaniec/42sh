051ccf8c0e032110eb4c06589cb075f74108d24e
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-10-27 16:23:33 +0200

    Correctly expanse aliases on one level

--- srcs/aliases_replace.c
+++ srcs/aliases_replace.c
@@ -6,32 +6,60 @@
 /*   By: sbrucker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/27 13:00:45 by sbrucker          #+#    #+#             */
-/*   Updated: 2018/10/27 14:02:00 by sbrucker         ###   ########.fr       */
+/*   Updated: 2018/10/27 16:21:00 by sbrucker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <forty_two_sh.h>
 
-static int	is_an_alias(const char *data, const struct s_alias *iterator)
+static struct s_alias	*is_an_alias(const char *data, const struct s_alias *iterator)
 {
 	while (iterator)
 	{
-		if (ft_strequ(data, iterator->key))
-			return (1);
+		if (iterator->value && iterator->value[0] && ft_strequ(data, iterator->key))
+			return ((struct s_alias *)iterator);
 		iterator = iterator->next;
 	}
-	return (0);
+	return (NULL);
 }
 
-static int	need_replace(const t_lexeme *lex, const struct s_alias *alias)
+static void		remplace(t_lexeme **lex, t_lexeme *last, t_lexeme **save, \
+				char *value)
 {
+	t_lexeme		*new;
+	t_lexeme		*next;
+
+	lexer(value, &new, NULL);
+	next = lex[0]->next;
+	if (!last)
+	{
+		*save = new;
+		*lex = new;
+	}
+	else
+		last->next = new;
+	while (new->next)
+		new = new->next;
+	new->next = next;
+}
+
+static t_lexeme	*need_replace(t_lexeme *lex, const struct s_alias *alias)
+{
+	struct s_alias	*to_remplace;
+	t_lexeme		*last;
+	t_lexeme		*save;
+
+	last = NULL;
+	save = lex;
 	while (lex)
 	{
-		if (is_an_alias(lex->data, alias))
-			return (1);
+		to_remplace = is_an_alias(lex->data, alias);
+		if (to_remplace)
+			remplace(&lex, last, &save, to_remplace->value);
+		last = lex;
 		lex = lex->next;
 	}
-	return (0);
+	return (save);
 }
 
 void	aliases_replace(t_lexeme **lex)
@@ -39,8 +67,5 @@ void	aliases_replace(t_lexeme **lex)
 	const struct s_alias *alias;
 
 	alias = access_alias_datas();
-	if (need_replace(*lex, alias))
-	{
-		dprintf(1, "There is an alias here\n");
-	}
+	*lex = need_replace(*lex, alias);
 }
