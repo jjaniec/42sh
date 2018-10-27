8faf05baf1e6c3f55f7cae11474e663d06b477b7
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-10-27 17:35:11 +0200

    Expansions changed, but tests are not okay anymore :(

--- srcs/exec/exec_pre_in_post.c
+++ srcs/exec/exec_pre_in_post.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 10:30:52 by sbrucker          #+#    #+#             */
-/*   Updated: 2018/10/13 20:04:32 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/10/27 17:30:31 by sbrucker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,46 @@ void	exec_argv(char **argv, t_exec *exe, t_ast *node)
 }
 
 /*
+** Determines if string should be passed to handle_quotes_expansions
+** to remove quotes / backslashes / replace expansion specifiers
+*/
+
+static int	is_clean_needed_expansions(char **data)
+{
+	char	*ptr;
+
+	ptr = EXPANSIONS_SPECIFIERS;
+	while (*ptr)
+		if (ft_strchr(*data, *ptr++))
+			return (1);
+	return (0);
+}
+
+
+static int	is_clean_needed(char **data)
+{
+	if (*data && (ft_strchr(*data, '\'') || \
+			ft_strchr(*data, '"') || ft_strchr(*data, '\\') || \
+			is_clean_needed_expansions(data)))
+		return (1);
+	return (0);
+}
+
+static void	clean_data(char **data)
+{
+	int		i;
+
+	i = 0;
+	while (data && data[i])
+	{
+		if (is_clean_needed(data + i))
+			handle_quotes_expansions(data + i);
+		//dprintf(1, "HERE: '%s'\n", data[i]);
+		i++;
+	}
+}
+
+/*
 ** Is executed at the first passage of a node in the AST
 */
 
@@ -52,6 +92,7 @@ t_exec	*in_exec(t_ast *node, t_exec *exe)
 {
 	if (!node->data)
 		return (exe);
+	clean_data(node->data);
 	log_debug("Current node IN : %s ready for exec %d", node->data[0], exe->ready_for_exec);
 	if (node->sub_ast)
 		script_in_exec(node->sub_ast, exe);
