0806201959d0bc7adff0ba7d80749e79cce5d0f6
Joffrey Janiec <jjaniec@student.42.fr>
2018-11-21 16:05:54 +0100

    Begin local vars assignements

--- srcs/exec/handle_env_assigns.c
+++ srcs/exec/handle_env_assigns.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 15:02:19 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/11/13 18:49:57 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/11/21 16:04:59 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,9 @@ static t_environ	*get_env_to_use(t_exec *exe, char ***tmp_env_assigns_begin)
 	*tmp_env_assigns_begin = NULL;
 	if (!(vars = get_shell_vars()))
 		return (NULL);
-	if (exe->prog_forked)
+	if (!exe)
+		return (vars->locals);
+	else if (exe->prog_forked)
 		*tmp_env_assigns_begin = \
 			vars->env->last_entry_ptr->ptr_to_pos_in_environ_tab + 1;
 	else
@@ -58,24 +60,27 @@ char		**handle_env_assigns(t_ast *node, t_exec *exe, \
 {
 	char		*tmp;
 	char		**tmp_env_assigns;
+	t_environ	*env_to_use;
 
-	(void)exe;
-	if (!(node && node->left && node->left->type == T_ENV_ASSIGN))
+	if (!(node && node->left && node->left->type == T_ENV_ASSIGN) && \
+		!(node->type != T_WORD && !exe))
 		return (NULL);
 	tmp_env_assigns = NULL;
-	while (node->left)
-		node = node->left;
+	while (node->left || (!exe && node->right))
+		node = (!exe && node->right) ? (node->right) : (node->left);
 	log_info("PID %zu: Handle env assigns of %s(t %d td %d)", getpid(), node->data[0], \
 		node->type, node->type_details);
-	*env_used = get_env_to_use(exe, &tmp_env_assigns);
+	env_to_use = get_env_to_use(exe, &tmp_env_assigns);
+	if (env_used)
+		*env_used = env_to_use;
 	while (node->type == T_ENV_ASSIGN)
 	{
-		if (!((*env_used)->get_var(*env_used, node->data[0])))
-			(*env_used)->add_var(*env_used, node->data[0], NULL);
+		if (!(env_to_use->get_var(env_to_use, node->data[0])))
+			env_to_use->add_var(env_to_use, node->data[0], NULL);
 		else
 		{
 			tmp = ft_strchr(node->data[0], '=');
-			ft_strncpy((*env_used)->last_used_elem->val_begin_ptr, tmp + sizeof(char), \
+			ft_strncpy(env_to_use->last_used_elem->val_begin_ptr, tmp + sizeof(char), \
 				MAX_ENV_ENTRY_LEN - ((tmp - node->data[0]) * sizeof(char)));
 		}
 		node = node->parent;
