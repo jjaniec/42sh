1121b47d85e48d5043d21f5d1a26b1816f579bcb
Lox3ur <cgaspart@student.42.fr>
2018-12-04 17:52:10 +0100

    Rm last_slash after double dot

--- srcs/builtin/cd/cd_l_dot_manager.c
+++ srcs/builtin/cd/cd_l_dot_manager.c
@@ -6,7 +6,7 @@
 /*   By: cgaspart <cgaspart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 11:57:23 by cgaspart          #+#    #+#             */
-/*   Updated: 2018/11/27 19:42:30 by cgaspart         ###   ########.fr       */
+/*   Updated: 2018/12/04 17:51:19 by cgaspart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,10 @@ static void	cd_back(t_cd *cd_i)
 
 	if (cd_i->env->get_var(cd_i->env, "PWD"))
 	{
-		tmp = cd_clean_last_slash(cd_i->env->last_used_elem->val_begin_ptr);
-		cd_i->cwd_link = cd_get_last_path(tmp);
+		tmp = cd_get_last_path(cd_i->env->last_used_elem->val_begin_ptr);
+		cd_i->cwd_link = cd_clean_last_slash(tmp);
+		//tmp = cd_clean_last_slash(cd_i->env->last_used_elem->val_begin_ptr);
+		//cd_i->cwd_link = cd_get_last_path(tmp);
 		free(tmp);
 		if (!cd_change_dir(cd_i->env, cd_i->cwd_link, cd_i->cwd))
 			link_env_update(cd_i);
