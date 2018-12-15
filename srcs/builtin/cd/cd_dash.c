78c59616316f675214328e3695ddc6e71ba590c3
Lox3ur <cgaspart@student.42.fr>
2018-12-15 13:31:00 +0100

    Last minor cd fixes

--- srcs/builtin/cd/cd_dash.c
+++ srcs/builtin/cd/cd_dash.c
@@ -6,42 +6,42 @@
 /*   By: cgaspart <cgaspart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 21:51:12 by cgaspart          #+#    #+#             */
-/*   Updated: 2018/12/14 21:54:44 by cgaspart         ###   ########.fr       */
+/*   Updated: 2018/12/15 13:30:28 by cgaspart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <forty_two_sh.h>
 
-static void	refresh_cd_dash(t_cd *cd_info)
+static void	refresh_cd_dash(t_cd *cd_info, char *env_old)
 {
-	char *old_oldpwd;
-
-	old_oldpwd = cd_clean_last_slash(
-	cd_info->env->last_used_elem->val_begin_ptr);
-	ft_putendl(old_oldpwd);
-	if (cd_check_link(cd_info, old_oldpwd))
+	ft_putendl(env_old);
+	if (cd_check_link(cd_info, env_old))
 	{
-		cd_info->cwd_link = old_oldpwd;
+		cd_info->cwd_link = env_old;
 		link_env_update(cd_info);
 	}
 	else
 	{
 		refresh_cwd_env(cd_info->env);
-		free(old_oldpwd);
+		free(env_old);
 	}
 }
 
 int			builtin_cd_dash(t_cd *cd_info)
 {
+	char *env_old;
+
 	if (cd_info->env->get_var(cd_info->env, "OLDPWD") &&
 	ft_strlen(cd_info->env->last_used_elem->val_begin_ptr) > 0)
 	{
+		env_old = ft_xstrdup(cd_info->env->last_used_elem->val_begin_ptr);
 		if (!cd_change_dir(cd_info->env,
 			cd_info->env->last_used_elem->val_begin_ptr, cd_info->cwd))
 		{
-			refresh_cd_dash(cd_info);
+			refresh_cd_dash(cd_info, env_old);
 			return (0);
 		}
+		free(env_old);
 	}
 	ft_putstr_fd(SH_NAME": cd: OLDPWD not set\n", 2);
 	return (1);
